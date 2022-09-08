818e6e15514bb4dfe811c25fb86bdfd98118d1e4
Franpan94 <francescopanico94@libero.it>
2022-09-08 16:16:41 +0200

    structure creation

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class PageController extends Controller
 {
@@ -10,8 +11,8 @@ class PageController extends Controller
     public function index(){
         
         $movies = Movie::all();
-        dd($movies);
-        return view('welcome');
+        
+        return view('home');
 
     }
 }
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+</head>
+<body>
+    <header>
+        @include('includes.header')
+    </header>
+
+    <main>
+        @yield('main-content')
+    </main>
+
+    <footer>
+        @include('includes.footer')
+    </footer>
+</body>
+</html>
