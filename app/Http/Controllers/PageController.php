2e371d802c76446da38d0cde38c51e390e019c22
Franpan94 <francescopanico94@libero.it>
2022-09-08 16:56:02 +0200

    loading database

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -12,7 +12,7 @@ class PageController extends Controller
         
         $movies = Movie::all();
         
-        return view('home');
+        return view('home', compact('movies'));
 
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.main')
 
 @section('main-content')
-ciao
+<div class="container">
+    <div class="row">
+        @foreach ($movies as $movie)
+         <div class="col-2 m-3">
+            <h3>{{ $movie -> title }}</h3>
+            <h4>{{ $movie -> nationality }}</h4>
+            <h5>{{ $movie -> date }}</h5>
+            <h6>{{ $movie -> vote }}</h6>
+         </div>
+        @endforeach
+    </div>
+    
+    
+        
+    
+</div>
 @endsection
